9ff5a06cae22aecd424362436a9c5f010d9e4fb7
zrnap <zrna.petr@gmail.com>
2023-01-24 11:13:53 +0100

    added landing image

---
+++ main.js
@@ -0,0 +1,12 @@
+function loadMain() {
+    document.getElementById("main-for-load").style.visibility = "visible";
+}
+
+document.addEventListener('click', function () {
+    document.getElementById("landing").style.opacity = "0";
+});
+
+setTimeout(loadMain, 1000);
+
+
+
