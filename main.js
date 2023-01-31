343b94a86fe6722e521491462e9990edee1baa5f
zrnap <zrna.petr@gmail.com>
2023-01-31 22:04:13 +0100

    small fix

--- main.js
+++ main.js
@@ -2,18 +2,9 @@ function scrollDown() {
     window.scrollBy(0, window.innerHeight);
 }
 
-window.addEventListener("scroll", function () {
-    const landingPage = document.querySelector(".landing-page");
-    if (window.scrollY > landingPage.offsetTop + landingPage.offsetHeight) {
-        landingPage.classList.add("hidden");
-    }
-});
-
 // window.addEventListener("scroll", function () {
 //     const landingPage = document.querySelector(".landing-page");
-//     if (window.scrollY > landingPage.offsetHeight) {
+//     if (window.scrollY > landingPage.offsetTop + landingPage.offsetHeight) {
 //         landingPage.classList.add("hidden");
 //     }
 // });
-
-10.2. 10: 00
